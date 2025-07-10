96edfbddce47defb3df4c8ae7bd39ff330e57ded
Fn-M <96444305+Fn-M@users.noreply.github.com>
2025-07-10 18:52:05 +0200

    Publish test

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    host: true
-  },
   base: '/Portfolio/',
-  css: {
-    postcss: './postcss.config.js'
-  }
 }) 
